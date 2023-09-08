1cdb213ee7e7386e4793820bd00f74270a243d6a
Chris Johansson <chris.johansson@mildmedia.se>
2023-09-08 11:16:34 +0200

    Add shared tailwind file

---
+++ shared/tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // Gutenberg - Wordpress
+    "./src/blocks/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // NextJS
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/blocks/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/utils/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      blue: "#374151",
+      blueDarker: "#232c3a",
+      pink: "#b250ba",
+      white: "#fff",
+    },
+    extend: {
+      fontFamily: {
+        heading: "var(--display-font)",
+        body: "var(--body-font)",
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [require("tailwind-children")],
+};
